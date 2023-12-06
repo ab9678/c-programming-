470dbfa65647c09bfec1a01bce3a490b5cb6a534
Abhigyan <abhigyanbhattacharya75@gmail.com>
2023-12-06 12:14:00 +0530

    updated folder name to make sense which year it was created

---
+++ nov2023/file.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+char location[30]; 
+char content[20];
+FILE *p;
+
+gets(content);
+
+//printf("specify location of the file ");
+//gets(location);
+p=fopen("/home/abhigyan/Documents/abc.txt","w");
+fputs(content,p);
+
+fclose(p);
+printf("Done!");
+printf("your file is ready\n");
+}
